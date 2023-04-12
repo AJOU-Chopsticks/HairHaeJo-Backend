62ef08d28fe34662c1d9760abceea8d825b1e435
LeeSeongGyu <lsg9706@naver.com>
2023-04-13 03:08:54 +0900

    git init

---
+++ src/test/java/Chopsticks/HairHaeJoBackend/HairHaeJoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Chopsticks.HairHaeJoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairHaeJoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
