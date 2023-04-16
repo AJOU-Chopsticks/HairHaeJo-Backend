6f1f5c3819c283154f74315d2cb310990ae312b8
LeeSeongGyu <lsg9706@naver.com>
2023-04-17 03:35:29 +0900

    [TEST] 회원가입 실패 테스트

--- src/test/java/Chopsticks/HairHaeJoBackend/user/UserControllerTest.java
+++ src/test/java/Chopsticks/HairHaeJoBackend/user/UserControllerTest.java
@@ -23,59 +23,108 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 class UserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("회원가입 성공")
-    void signup() throws Exception{
-        //given
-        ObjectMapper objectMapper = new ObjectMapper();
-        String object = objectMapper.writeValueAsString(SignupRequestDto.builder()
-            .email("user@example.com")
-            .password("password")
-            .name("user")
-            .phoneNumber("01012345678")
-            .gender(1)
-            .age(20)
-            .build());
+	@Test
+	@DisplayName("회원가입 성공")
+	void signup() throws Exception {
+		//given
+		ObjectMapper objectMapper = new ObjectMapper();
+		String object = objectMapper.writeValueAsString(SignupRequestDto.builder()
+			.email("user@example.com")
+			.password("password")
+			.name("user")
+			.phoneNumber("01012345678")
+			.gender(1)
+			.age(20)
+			.build());
 
-        //when
-        ResultActions actions = mockMvc.perform(post("/user/signup")
-            .content(object)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON));
+		//when
+		ResultActions actions = mockMvc.perform(post("/user/signup")
+			.content(object)
+			.accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON));
 
-        //then
-        actions.andExpect(status().isOk());
-    }
+		//then
+		actions.andExpect(status().isOk());
+	}
 
-    @Test
-    @DisplayName("회원가입 실패: 중복된 가입정보")
-    void signup_duplicated_email() throws Exception{
-        //given
-        ObjectMapper objectMapper = new ObjectMapper();
-        String object = objectMapper.writeValueAsString(SignupRequestDto.builder()
-            .email("user@example.com")
-            .password("password")
-            .name("user1")
-            .phoneNumber("01011111111")
-            .gender(1)
-            .age(20)
-            .build());
+	@Test
+	@DisplayName("회원가입 실패: 중복된 이메일")
+	void signup_duplicated_email() throws Exception {
+		//given
+		ObjectMapper user1 = new ObjectMapper();
+		String object1 = user1.writeValueAsString(SignupRequestDto.builder()
+			.email("user1@example.com")
+			.password("password")
+			.name("user1")
+			.phoneNumber("01011111111")
+			.gender(1)
+			.age(20)
+			.build());
 
-        //when
-        mockMvc.perform(post("/user/signup")
-            .content(object)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON));
+		ObjectMapper user2 = new ObjectMapper();
+		String object2 = user2.writeValueAsString(SignupRequestDto.builder()
+			.email("user1@example.com")
+			.password("password")
+			.name("user2")
+			.phoneNumber("01022222222")
+			.gender(1)
+			.age(20)
+			.build());
 
-        ResultActions actions = mockMvc.perform(post("/user/signup")
-            .content(object)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON));
+		//when
+		mockMvc.perform(post("/user/signup")
+			.content(object1)
+			.accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON));
 
-        //then
-        actions.andExpect(status().is4xxClientError());
-    }
+		ResultActions actions = mockMvc.perform(post("/user/signup")
+			.content(object2)
+			.accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON));
+
+		//then
+		actions.andExpect(status().is4xxClientError());
+	}
+
+	@Test
+	@DisplayName("회원가입 실패: 중복된 전화번호")
+	void signup_duplicated_phoneNumber() throws Exception {
+		//given
+		ObjectMapper user1 = new ObjectMapper();
+		String object1 = user1.writeValueAsString(SignupRequestDto.builder()
+			.email("user1@example.com")
+			.password("password")
+			.name("user1")
+			.phoneNumber("01011111111")
+			.gender(1)
+			.age(20)
+			.build());
+
+		ObjectMapper user2 = new ObjectMapper();
+		String object2 = user2.writeValueAsString(SignupRequestDto.builder()
+			.email("user2@example.com")
+			.password("password")
+			.name("user2")
+			.phoneNumber("01011111111")
+			.gender(1)
+			.age(20)
+			.build());
+
+		//when
+		mockMvc.perform(post("/user/signup")
+			.content(object1)
+			.accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON));
+
+		ResultActions actions = mockMvc.perform(post("/user/signup")
+			.content(object2)
+			.accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON));
+
+		//then
+		actions.andExpect(status().is4xxClientError());
+	}
 }
