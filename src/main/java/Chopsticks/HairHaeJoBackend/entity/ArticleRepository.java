c41ab3e3daec99f87b25ada72e5fa0d48a6358da
HongGunhwa <dundae@ajou.ac.kr>
2023-04-25 19:57:24 +0900

    게시글 삭제/수정

--- src/main/java/Chopsticks/HairHaeJoBackend/entity/ArticleRepository.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/entity/ArticleRepository.java
@@ -1,8 +1,19 @@
 package Chopsticks.HairHaeJoBackend.entity;
 
+import java.io.IOException;
+import java.util.Collection;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+    public Collection<Article> findbywriterAndstate(Long writer,Articlestate articlestate);
+    @Modifying // select 문이 아님을 나타낸다
+    @Query(value = "UPDATE Article A set A.title = :title, A.body = :body,A.before_image=:beforeimage, A.after_image=:afterimage,A.abstraction_location=:location,A.category=:category where A.writer_id = :writer_id And A.state=:state", nativeQuery = true)
+    void changeArticle(@Param("title")String title, @Param("body")String body,
+                   @Param("beforeimage")String beforeimage,@Param("afterimage")String afterimage,
+                   @Param("location")String location,@Param("category")String category,
+                   @Param("writer_id")Long writer_id,@Param("state")Articlestate state) throws IOException;
 }
--- src/main/java/Chopsticks/HairHaeJoBackend/service/ArticleService.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/service/ArticleService.java
@@ -1,12 +1,13 @@
 package Chopsticks.HairHaeJoBackend.service;
 
 import Chopsticks.HairHaeJoBackend.dto.article.MakeArticleDto;
+import Chopsticks.HairHaeJoBackend.entity.Article;
 import Chopsticks.HairHaeJoBackend.entity.ArticleRepository;
 
+import Chopsticks.HairHaeJoBackend.entity.Articlestate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 
 @Service
@@ -14,20 +15,47 @@ import java.io.IOException;
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final S3UploadService s3UploadService;
-    public void post(MultipartFile before, MultipartFile after, MakeArticleDto articleDto, Long currentMemberId) throws IOException {
-
+    public int post(MultipartFile before, MultipartFile after, MakeArticleDto articleDto, Long currentMemberId) throws IOException {
+        Article nowarticle=null;
         if (before.isEmpty()){
             if(after.isEmpty()) {
-                articleRepository.save(articleDto.toArticle(currentMemberId,null,null));
+                nowarticle=articleRepository.save(articleDto.toArticle(currentMemberId,null,null));
             }
-            else articleRepository.save(articleDto.toArticle(currentMemberId,null,s3UploadService.upload(after)));
+            else nowarticle=articleRepository.save(articleDto.toArticle(currentMemberId,null,s3UploadService.upload(after)));
         }
         else {
             if(after.isEmpty()){
-                articleRepository.save(articleDto.toArticle(currentMemberId,s3UploadService.upload(before),null));
+                nowarticle=articleRepository.save(articleDto.toArticle(currentMemberId,s3UploadService.upload(before),null));
             }
-            else articleRepository.save(articleDto.toArticle(currentMemberId,s3UploadService.upload(before),s3UploadService.upload(after)));
+            else nowarticle=articleRepository.save(articleDto.toArticle(currentMemberId,s3UploadService.upload(before),s3UploadService.upload(after)));
         }
+        if(nowarticle!=null) {
+            return nowarticle.getId();
+        }
+        else {
+            return -1;
+        }
+
+    }
+    public void retouch(MultipartFile before, MultipartFile after, MakeArticleDto articleDto, Long currentMemberId) throws IOException {
+        Article nowarticle=null;
+        articleRepository.findbywriterAndstate(currentMemberId, Articlestate.WATING);
+
+        if (before.isEmpty()){
+            if(after.isEmpty()) {
+                articleRepository.changeArticle(articleDto.getTitle(), articleDto.getBody(),null,null,articleDto.getRegion(), articleDto.getCategory(), currentMemberId,Articlestate.WATING);
+            }
+            else articleRepository.changeArticle(articleDto.getTitle(), articleDto.getBody(),null,s3UploadService.upload(after),articleDto.getRegion(), articleDto.getCategory(), currentMemberId,Articlestate.WATING);
+        }
+        else {
+            if(after.isEmpty()){
+                articleRepository.changeArticle(articleDto.getTitle(), articleDto.getBody(),s3UploadService.upload(before),null,articleDto.getRegion(), articleDto.getCategory(), currentMemberId,Articlestate.WATING);
+            }
+            else articleRepository.changeArticle(articleDto.getTitle(), articleDto.getBody(),s3UploadService.upload(before),s3UploadService.upload(after),articleDto.getRegion(), articleDto.getCategory(), currentMemberId,Articlestate.WATING);
+        }
+    }
+    public void delete(int currentArticleId) throws IOException {
+        articleRepository.deleteById(Integer.toUnsignedLong(currentArticleId));
 
     }
 }
