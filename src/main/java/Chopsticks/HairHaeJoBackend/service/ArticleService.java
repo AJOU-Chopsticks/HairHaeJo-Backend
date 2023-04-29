02f4cd8b7f03eb52101f8b4b2cb5034a9ef0686a
HongGunhwa <dundae@ajou.ac.kr>
2023-04-29 17:49:35 +0900

    게시글 작성 중복확인 기능 추가

--- src/main/java/Chopsticks/HairHaeJoBackend/service/ArticleService.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/service/ArticleService.java
@@ -1,6 +1,7 @@
 package Chopsticks.HairHaeJoBackend.service;
 
 import Chopsticks.HairHaeJoBackend.dto.article.ChangeArticleDto;
+import Chopsticks.HairHaeJoBackend.dto.article.DeleteArticleDto;
 import Chopsticks.HairHaeJoBackend.dto.article.MakeArticleDto;
 import Chopsticks.HairHaeJoBackend.entity.Article;
 import Chopsticks.HairHaeJoBackend.entity.ArticleRepository;
@@ -13,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
-import java.util.Collection;
 
 
 @Service
@@ -26,6 +26,8 @@ public class ArticleService {
     public String post(MultipartFile before, MultipartFile after, MakeArticleDto articleDto, Long currentMemberId) throws IOException {
         Article nowarticle;
         String beforeurl,afterurl;
+        if(articleRepository.thereiswrote(currentMemberId,Articlestate.WATING)!=0)
+            throw new RuntimeException("이미 대기중인 작성글이 존재합니다");
 
         if (before==null) beforeurl=null;
         else beforeurl=s3UploadService.upload(before);
@@ -34,7 +36,10 @@ public class ArticleService {
         else afterurl= s3UploadService.upload(after);
 
         nowarticle = articleRepository.save(articleDto.toArticle(currentMemberId, beforeurl, afterurl));
-        return Integer.toString(nowarticle.getId());
+        DeleteArticleDto returndata=new DeleteArticleDto(Integer.toString(nowarticle.getId()));
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new SimpleModule());
+        String articlenumber=objectMapper.writeValueAsString(returndata);
+        return articlenumber;
     }
 
     public void retouch(MultipartFile before, MultipartFile after, ChangeArticleDto articleDto) throws IOException {
