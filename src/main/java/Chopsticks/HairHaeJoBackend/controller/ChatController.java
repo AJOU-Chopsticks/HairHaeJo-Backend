a2863092f77b581069178fe8e3c0e33c2d6c1559
LeeSeongGyu <lsg9706@naver.com>
2023-05-01 20:13:24 +0900

    [FIX] 채팅 나가기 오류 수정

--- src/main/java/Chopsticks/HairHaeJoBackend/controller/ChatController.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/controller/ChatController.java
@@ -74,6 +74,7 @@ public class ChatController {
     //메시지 전송
     @MessageMapping("/chat/message")
     public void message(ChatMessageRequestDto message) {
+        System.out.println("*********test**********");
         template.convertAndSend("/sub/chat/" + message.getRoomId(),
             chatService.saveMessage(message));
     }
