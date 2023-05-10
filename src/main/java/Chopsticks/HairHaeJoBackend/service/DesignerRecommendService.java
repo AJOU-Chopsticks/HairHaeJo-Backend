b32c95b6b97be0aefbdff08bc26c21a79cb03a1d
LeeSeongGyu <lsg9706@naver.com>
2023-05-11 01:16:34 +0900

    [FEAT]디자이너 추천 API

---
+++ src/main/java/Chopsticks/HairHaeJoBackend/service/DesignerRecommendService.java
@@ -0,0 +1,82 @@
+package Chopsticks.HairHaeJoBackend.service;
+
+import Chopsticks.HairHaeJoBackend.dto.designer.RecommendResponseDto;
+import Chopsticks.HairHaeJoBackend.entity.user.User;
+import Chopsticks.HairHaeJoBackend.entity.user.UserRepository;
+import Chopsticks.HairHaeJoBackend.jwt.SecurityUtil;
+import Chopsticks.HairHaeJoBackend.entity.designer.DesignerRecommendRepository;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DesignerRecommendService {
+
+    private final DesignerRecommendRepository designerRecommendRepository;
+    private final UserRepository userRepository;
+
+    public List<RecommendResponseDto> getRecommendDesigners(String region) {
+        User user = getCurrentUser();
+        List<Long> recommendDesignerIdList = getRecommendDesignerIdList(user.getGender(),
+            user.getAge(), region);
+        List<RecommendResponseDto> list = new ArrayList<>();
+
+        for (Long id : recommendDesignerIdList) {
+            User designer = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("디자이너 정보가 없습니다." ));
+            RecommendResponseDto responseDto = RecommendResponseDto.builder()
+                .designerId(designer.getId())
+                .Name(designer.getName())
+                .ProfileImage(designer.getProfileImage()).build();
+            list.add(responseDto);
+        }
+
+        return list;
+    }
+
+    private List<Long> getRecommendDesignerIdList(int gender, int age, String region) {
+        HashMap<Long, Long> count = new HashMap<>();
+
+        List<Long> designers = designerRecommendRepository.getDesignersByRegion(region);
+        for (Long id : designers) {
+            count.put(id, 0L);
+        }
+
+        List<Object[]> reservation = designerRecommendRepository.countReservation(gender, age,
+            region);
+        for (Object[] obj : reservation) {
+            Long id = Long.parseLong(String.valueOf(obj[0]));
+            Long cnt = Long.parseLong(String.valueOf(obj[1]));
+            count.put(id, count.get(id) + cnt * 5);
+        }
+
+        List<Object[]> like = designerRecommendRepository.countLike(gender, age, region);
+        for (Object[] obj : like) {
+            Long id = Long.parseLong(String.valueOf(obj[0]));
+            Long cnt = Long.parseLong(String.valueOf(obj[1]));
+            count.put(id, count.get(id) + cnt);
+        }
+
+        List<Object[]> review = designerRecommendRepository.countReview(gender, age, region);
+        for (Object[] obj : review) {
+            Long id = Long.parseLong(String.valueOf(obj[0]));
+            Long cnt = Long.parseLong(String.valueOf(obj[1]));
+            count.put(id, count.get(id) + cnt * 10);
+        }
+
+        List<Long> designerList = new ArrayList<>(count.keySet());
+        Collections.sort(designerList, (o1, o2) -> (int) (count.get(o2) - count.get(o1)));
+
+        return designerList;
+    }
+
+    private User getCurrentUser() {
+        User user = userRepository.findById(SecurityUtil.getCurrentMemberId())
+            .orElseThrow(() -> new RuntimeException("로그인 정보가 없습니다." ));
+        return user;
+    }
+}
