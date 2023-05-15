716444e7003e56d3c8739588006a6dbc715e809d
HongGunhwa <dundae@ajou.ac.kr>
2023-05-15 18:14:06 +0900

    예약 가능 시간 체크 디버깅

--- src/main/java/Chopsticks/HairHaeJoBackend/controller/ReservationController.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/controller/ReservationController.java
@@ -8,10 +8,18 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,14 +28,13 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @GetMapping("")
-    public ResponseEntity<APIMessages> viewReservation(@RequestParam(value = "designerId") String designerId,@RequestParam(value = "reservationday") String nowday) throws IOException
+    public ResponseEntity<APIMessages> viewReservation(@RequestParam(value = "designerId") String designerId, @RequestParam(value = "reservationday") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) throws IOException, ParseException {
 
-    {
-
-
+        LocalDateTime localDateTime1 = date.atStartOfDay();
+        LocalDateTime localDateTime2 = date.plusDays(1).atStartOfDay();
         APIMessages apiMessages=APIMessages.builder().success(true)
                 .message("예약 가능 시간 데이터")
-                .data(reservationService.viewReservationDay(Long.parseLong(designerId),nowday))
+                .data(reservationService.viewReservationDay(Long.parseLong(designerId),localDateTime1,localDateTime2))
                 .build();
         return ResponseEntity.ok(apiMessages);
     }
