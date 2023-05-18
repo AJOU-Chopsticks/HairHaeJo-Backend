5a10522ac909522c5c28577bf557622aa2f0dab7
HongGunhwa <dundae@ajou.ac.kr>
2023-05-18 19:54:34 +0900

    예약 불가능 시간 전송 리팩토링

--- src/main/java/Chopsticks/HairHaeJoBackend/service/ReservationService.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/service/ReservationService.java
@@ -2,12 +2,13 @@ package Chopsticks.HairHaeJoBackend.service;
 
 
 import Chopsticks.HairHaeJoBackend.dto.reservation.PossibleDayResponse;
-import Chopsticks.HairHaeJoBackend.dto.reservation.PossibleTimeResponse;
+import Chopsticks.HairHaeJoBackend.dto.reservation.ImPossibleTimeResponse;
 import Chopsticks.HairHaeJoBackend.dto.reservation.ReserveListDto;
 import Chopsticks.HairHaeJoBackend.entity.menu.DesignerMenuRepository;
 import Chopsticks.HairHaeJoBackend.entity.reservation.ReservationRepository;
 import Chopsticks.HairHaeJoBackend.entity.user.UserRepository;
 import Chopsticks.HairHaeJoBackend.repository.DesignerProfileRepository;
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,20 +28,23 @@ public class ReservationService {
     private DesignerProfileRepository designerProfileRepository;
     private UserRepository userRepository;
 
-    public ArrayList<String> viewReservationDay(long designerId, LocalDateTime day1, LocalDateTime day2) {
+    public ArrayList<ImPossibleTimeResponse> viewReservationDay(long designerId, LocalDateTime day1, LocalDateTime day2) {
         List<PossibleDayResponse> list=reservationRepository.PossibleDay(designerId,day1,day2);
         ListIterator<PossibleDayResponse> iterator = list.listIterator();
         //정방향 출력
-        ArrayList<String> time=new ArrayList<>();
-
-        while(iterator.hasNext()){
-            PossibleDayResponse x=iterator.next();
-            String nowTime=x.getStart().format(DateTimeFormatter.ofPattern("HH-mm"));
-            System.out.println(nowTime);
-            PossibleTimeResponse now=new PossibleTimeResponse(nowTime);
-            time.add(nowTime);
-
+        ArrayList<ImPossibleTimeResponse> time=new ArrayList<>();
+        day1=day1.plusHours(11);
+        while(iterator.hasNext()) {
+            PossibleDayResponse x = iterator.next();
+            if(day1.isBefore(x.getStart())) day1=x.getStart();
+            while (!day1.isEqual(x.getEnd())) {
+                    JsonObject temp = new JsonObject();
+                    String nowTime=day1.format(DateTimeFormatter.ofPattern("HH-mm"));
+                    time.add(new ImPossibleTimeResponse(nowTime));
+                    day1=day1.plusMinutes(30);
+                }
         }
+
         return time;
     }
 
