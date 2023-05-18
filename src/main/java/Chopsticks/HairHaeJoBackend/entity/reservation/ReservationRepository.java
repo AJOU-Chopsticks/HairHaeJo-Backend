9fa9e97aeadc1bf65777f9773456419119183a89
HongGunhwa <dundae@ajou.ac.kr>
2023-05-18 19:54:34 +0900

    예약 불가능 시간 전송 리팩토링

--- src/main/java/Chopsticks/HairHaeJoBackend/entity/reservation/ReservationRepository.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/entity/reservation/ReservationRepository.java
@@ -14,8 +14,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Intege
 
     Reservation findBytid(String tid);
 
-    @Query(value = "SELECT distinct new Chopsticks.HairHaeJoBackend.dto.reservation.PossibleDayResponse(R.startTime)" +
-            "FROM Reservation R WHERE R.designerId=:id And R.startTime>=:date1 And R.startTime<=:date2")
+    @Query(value = "SELECT distinct new Chopsticks.HairHaeJoBackend.dto.reservation.PossibleDayResponse(R.startTime,R.endTime)" +
+            "FROM Reservation R WHERE R.designerId=:id And R.startTime>=:date1 And R.startTime<=:date2 ORDER BY R.startTime ASC")
     List<PossibleDayResponse> PossibleDay(@Param(value="id") long designerId, LocalDateTime date1, LocalDateTime date2);
 
 
