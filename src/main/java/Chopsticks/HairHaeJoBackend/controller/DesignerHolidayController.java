a1284bcdc07fd55ec4ca9a2be5b04ed21d0f3078
HongGunhwa <dundae@ajou.ac.kr>
2023-05-25 20:32:12 +0900

    디자이너 휴일 조회/변경/등록

---
+++ src/main/java/Chopsticks/HairHaeJoBackend/controller/DesignerHolidayController.java
@@ -0,0 +1,62 @@
+package Chopsticks.HairHaeJoBackend.controller;
+
+import Chopsticks.HairHaeJoBackend.dto.APIMessages;
+import Chopsticks.HairHaeJoBackend.dto.holiday.HolidayDto;
+import Chopsticks.HairHaeJoBackend.service.DesignerHolidayService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping("/crm/holiday")
+public class DesignerHolidayController {
+
+    private final DesignerHolidayService designerHolidayService;
+
+    @PostMapping("")
+    public ResponseEntity<APIMessages> PostHoliday(@RequestParam("jsonList") String jsonList) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new SimpleModule());
+        HolidayDto holidayDto = objectMapper.readValue(jsonList, new TypeReference<>() {});
+
+        designerHolidayService.post(holidayDto);
+        APIMessages apiMessages=APIMessages.builder().success(true)
+                .message("휴일 작성 성공")
+                .build();
+        return ResponseEntity.ok(apiMessages);
+
+    }
+
+    @PutMapping("")
+    public ResponseEntity<APIMessages> ChangeHoliday(@RequestParam("jsonList") String jsonList) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new SimpleModule());
+        HolidayDto holidayDto = objectMapper.readValue(jsonList, new TypeReference<>() {});
+
+        designerHolidayService.change(holidayDto);
+        APIMessages apiMessages=APIMessages.builder().success(true)
+                .message("휴일 변경 성공")
+                .build();
+        return ResponseEntity.ok(apiMessages);
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<APIMessages> viewHoliday(@RequestParam("DesignerId") long DesignerId) throws IOException {
+
+        APIMessages apiMessages=APIMessages.builder().success(true)
+                .message("휴일 조회 성공")
+                .data( designerHolidayService.view(DesignerId))
+                .build();
+        return ResponseEntity.ok(apiMessages);
+
+    }
+
+}
--- src/main/java/Chopsticks/HairHaeJoBackend/entity/article/ArticleRepository.java
+++ src/main/java/Chopsticks/HairHaeJoBackend/entity/article/ArticleRepository.java
@@ -8,9 +8,10 @@ import Chopsticks.HairHaeJoBackend.dto.article.ArticlelistResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer>, ArticleRepositoryCustom {
 
 
